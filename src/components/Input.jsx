2168618d50e4312a6b8a249c9a457fd5d9cdcb9c
Madalin Manceriu <58183823+ManceriuMadalin@users.noreply.github.com>
2023-10-09 14:45:53 +0300

    Delete src/components directory

--- src/components/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function Input() {
-     return (
-          <input type="text" placeholder="Add your task for today" />
-     )
-}
--- src/components/function.js
+++
@@ -1,7 +0,0 @@
-export function addTask() {
-     return 0
-}
-
-export function deleteTask() {
-     return 0
-}
